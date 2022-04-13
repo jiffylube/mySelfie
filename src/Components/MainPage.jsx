0ce40fd738851ebe5587e5808759cdcf02b5fc42
ehlee95 <eric95lee@gmail.com>
2022-04-13 16:32:20 -0400

    made landing page

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,4 +1,31 @@
+import { useRef } from "react";
+
 function Main() {
+
+  const videoRef = useRef(null);
+
+  // Promise.all([
+  //   faceapi.nets.tnyFaceDetector.loadFromUri("/models"),
+  //   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  //   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  //   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  // ]);
+
+
+  function startVideo() {
+    navigator.mediaDevices
+      .getUserMedia({ video: {} })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.err("error", err);
+      });
+  }
+
+  startVideo();
 
   return (
     <div className="App">
