5d8874dcfd4dacd9a8d291faa6d0c21cbddb3a2b
jiffylube <lu.jeffrey91@gmail.com>
2022-04-13 14:38:00 -0400

    added webcam

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+
+  const videoRef = useRef(null);
+
+  function startVideo() {
+    navigator.mediaDevices.getUserMedia(
+      { video: {} })
+        .then(stream => {
+          let video = videoRef.current;
+          video.srcObject = stream;
+          video.play();
+        })
+        .catch(err => {
+        console.err("error" , err)
+      })
+  }
+
+  startVideo();
+
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome - Bro nana
-      </header>
+
+      <div>
+          {/* <video ref={videoRef} autoPlay muted></video> */}
+      </div>
+
     </div>
   );
 }
