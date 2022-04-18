74076c55903e402911865006a99a2d017fcff379
ehlee95 <eric95lee@gmail.com>
2022-04-18 11:09:08 -0400

    login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,6 @@ import { useNavigate, Link, Routes, Route } from 'react-router-dom';
 import Main from "./MainPage"
 import axios from "axios";
 
-
 function Login() {
 
   let accounts = [];
@@ -14,36 +13,35 @@ function Login() {
 
     e.preventDefault();
 
-    // change to useState
-    let email = e.target[0].value;
-    let password = e.target[1].value;
-    let match = false;
+      let email = e.target[0].value;
+      let password = e.target[1].value;
+      let match = false;
 
-    // get all accounts from api
-    axios.get("https://backend-fr.herokuapp.com/api/users")
-      .then((results) => {
-        results.data.forEach((user) => {
-          accounts.push(user)
+      // get all accounts from api
+      axios.get("https://backend-fr.herokuapp.com/api/users")
+        .then((results) => {
+          results.data.forEach((user) => {
+            accounts.push(user)
+          })
         })
-      })
-      // see if email is in the users database
-      .then(() => {
-        accounts.forEach((account) => {
-          // if email matches, check if password matches
-          if (email === account.email) {
-            if (password === account.password) {
-              // if password matches, redirect to main page
-              console.log("valid account")
-              match = true;
-              navigate("/mainpage");
+        // see if email is in the users database
+        .then(() => {
+          accounts.forEach((account) => {
+            // if email matches, check if password matches
+            if (email === account.email) {
+              if (password === account.password) {
+                // if password matches, redirect to main page
+                console.log("valid account")
+                match = true;
+                navigate("/mainpage");
+              }
             }
+          })
+          if (!match) {
+            // otherwise say invalid username/pw
+            console.log("invalid username/password")
           }
         })
-        if (!match) {
-          // otherwise say invalid username/pw
-          alert("invalid username/password")
-        }
-      })
   }
 
   return (
@@ -53,15 +51,9 @@ function Login() {
           <p className="title">Log in</p>
         </div>
         <form className="loginForm" onSubmit={handleSubmit}>
-          <input className="loginInput" placeholder="UserName"></input>
+          <input className="loginInput" placeholder="Username"></input>
           <input className="loginInput" type="password" placeholder="Password"></input>
-
-          <input className="loginButton" type="submit" value="Login" />
-
-          {/* <Link to={"/mainpage"} className="loginButton">Login</Link> */}
-          <Routes>
-            <Route path="/mainpage" element={<Main />} />
-          </Routes>
+          <input className="loginButton" type="submit"/>
         </form>
 
       </div>
