c0e658a043ebae27572a3bf7963d2ebe8561ee66
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-04-18 15:16:06 -0400

    setting up the photo on the main page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import Landing from "./Components/Landing";
-import Main from "./Components/MainPage";
+import Faceapi from "./Components/Faceapi";
+// import Main from "./Components/MainPage";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
+      {/* <Faceapi /> */}
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/mainpage" element={<Main />} />
+          <Route path="/mainpage" element={<Faceapi />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
