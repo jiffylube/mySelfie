dc38904af42d7a33310e0c39ab6196027a30d897
jiffylube <lu.jeffrey91@gmail.com>
2022-04-18 21:15:39 -0400

    css mainpage

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,51 +1,66 @@
-import "./MainPage.css"
-import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import * as faceapi from 'face-api.js'
+// Importing dependencies
+import React, { useRef, useEffect, useState } from "react"
+import './MainPage.css'
 
-function Main() {
-
-  // Starting video
+// Function
+function Faceapi({ currentEmail }) {
   const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const [hasPhoto, setHasPhoto] = useState(false);
 
-  function startVideo() {
-    navigator.mediaDevices
-      .getUserMedia({ video: {} })
-      .then((stream) => {
+  // Enable camera
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: { width: 1920, height: 1080 }
+    })
+      .then(stream => {
         let video = videoRef.current;
         video.srcObject = stream;
         video.play();
       })
-      .catch((err) => {
-        console.log("error", err);
-      });
+      .catch(err => {
+        console.error(err);
+      })
   }
 
-  startVideo();
+  useEffect(() => {
+    getVideo();
+  }, [videoRef])
 
-  // Calling models to run in paralel
-  
- 
+  // Take photo
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16 / 9)
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext('2d');
+    ctx.drawImage(video, 0, 0, width, height);
+    setHasPhoto(true);
+  }
+
+  useEffect(() => {
+    console.log(currentEmail);
+  })
+
   return (
-    <div>
-      <div>
-        <Link to={"/"}>Logout</Link>
+    <div className="viewPort">
+      <div className="camera">
+        <video className="displayArea" ref={videoRef}></video>
+        <button className="pictureButton" onClick={takePhoto}>SNAP!</button>
       </div>
-      <div className="wrapperMain">
-        <div className="leftMain">
-          <video className="webcamMain" ref={videoRef} autoPlay muted></video>
-          <button className="captureMain">Capture</button>
-        </div>
-        <div className="rightMain">
-          <div className="snapsMain">
-            <div className="snapMain"></div>
-            <div className="snapMain"></div>
-            <div className="snapMain"></div>
-          </div>
-        </div>
+      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+        <canvas className="theCanvas" ref={photoRef}></canvas>
+        <button className="pictureButton">CLOSE!</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Main;
+// Export
+export default Faceapi
